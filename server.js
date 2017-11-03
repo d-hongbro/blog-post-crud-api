f0fd6c7a9efbe6ac588548349f7a2ecdd8c294cb
David Hong <davidhong342@gmail.com>
2017-11-03 12:12:49 -0700

    fixed closed server

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
+
+const app = express();
 
 const blogPostRouter = require('./blogPostRouter');
-
-const jsonParser = bodyParser.json();
-const app = express();
 
 // log the http layer
 app.use(morgan('common'));
@@ -54,11 +51,6 @@ function closeServer() {
     });
   });
 }
-
-
-// app.listen(process.env.PORT || 8080, () => {
-//   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-// });
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
